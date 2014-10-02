05c479aa18e75bb9d5aaf4f482be297405ec7f1d
karata <karata.brown@outlook.com>
2014-10-02 18:49:18 +0800

    'init'

---
+++ Reluctant/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Reluctant.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Reluctant/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Reluctant.WebApiApplication" Language="C#" %>
